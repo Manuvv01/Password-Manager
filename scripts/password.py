b8e39939a90f142e74b72e9f40b06a1dc72b07eb
Manuel Vela <manuvvela@gmail.com>
2025-06-05 15:42:10 -0500

    Change keys(I leaked the key T.T)

--- scripts/password.py
+++ scripts/password.py
@@ -4,6 +4,7 @@ import random, pyperclip, json
 
 def main(window):
 
+
     #Password function
     def password_gen():
 
--- test.py
+++ test.py
@@ -1,26 +1,10 @@
 from cryptography.fernet import Fernet
 import json
 # OPEN KEY IN USB
-access_key = b"VNTV08oQcxuwDcKbuRDVdOa6v6LzaORKGOg7E3357Z8="
+access_key = b""
 # DECRYPT
+
 f = Fernet(access_key)
-# READ
-with open("stargate.json", "rb") as x:
-    encrypt_file = x.read()
-
-decrypt_file = f.decrypt(encrypt_file)
-
-with open("huh.json", "wb") as a:
-    a.write(decrypt_file)
-
-with open("huh.json", "r") as y:
-    data = json.load(y)
-
-key = data.get("link-start")
-data_id = data.get("cookie")
-name = data.get("owner")
-
-print(f"Name: {name}/Key:{key}/ID:{data_id}")
 
 
 
